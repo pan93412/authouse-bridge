4c791aafaeaf115aabf67361d2bd1ee95fd53a3c
Yi-Jyun Pan <pan93412@gmail.com>
2021-04-28 12:16:53 +0800

    fix(controller/ports/list): update the log

--- src/controllers/ports/list.ts
+++ src/controllers/ports/list.ts
@@ -3,6 +3,6 @@ import { listPorts } from "../../services/ports/list";
 
 export default function PostsList(fastify: FastifyInstance): void {
     fastify.get("/ports", async function (request, reply) {
-        return listPorts(request.log.child({ from: '/ports/list' }));
+        return listPorts(request.log.child({ from: '/ports' }));
       });
 }
