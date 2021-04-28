0bf197179ef972297e2dc2cf3a64badb1d4933e8
Yi-Jyun Pan <pan93412@gmail.com>
2021-04-28 12:07:49 +0800

    fix(interact): can interact correctly

--- src/controllers/ports/interact.ts
+++ src/controllers/ports/interact.ts
@@ -12,19 +12,21 @@ function isIParams(val: unknown): val is IParams {
   return typeof v.path === "string";
 }
 
-// TODO: 多人登入或是還沒完全關閉的情況可能會掛掉。需要做個 Pool。
 export default function PostsWrite(fastify: FastifyInstance): void {
   fastify.get("/ports/:path", { websocket: true }, async function (connection, request) {
     if (isIParams(request.params)) {
       const path = await resolvePort(request.params.path);
 
       if (!path) {
+        request.log.info(`the path parameter client sent didn't exist.`);
         connection.socket.close(WSCustomCloseCode.PARAM_NOT_EXIST);
         return;
       }
 
       const serialPort = new SerialPort(path, { baudRate: 9600 }, (e) => {
         if (e) {
+          request.log.warn(`failed to start the serial port.`);
+          if (e) request.log.warn(`reason: ${e.name} ${e.message}`);
           connection.socket.close(WSCustomCloseCode.FAILED_TO_OPEN);
           return;
         }
@@ -34,6 +36,7 @@ export default function PostsWrite(fastify: FastifyInstance): void {
   
       connection.socket.on("message", (data) => {
         serialPort.write(data as Buffer, (error, bytesWritten) => {
+          request.log.info(`forwarded (SEND) ${data} from the client to the server.`);
           connection.socket.send(JSON.stringify({
             type: InteractMode.SEND,
             error,
@@ -44,7 +47,7 @@ export default function PostsWrite(fastify: FastifyInstance): void {
 
       parser.on("data", (data: Buffer | string) => {
         const dataString = data.toString();
-
+        request.log.info(`forwarded (RECV) ${dataString} from the server to the client.`);
         connection.socket.send(JSON.stringify({
           type: InteractMode.RECV,
           data: dataString,
@@ -52,14 +55,23 @@ export default function PostsWrite(fastify: FastifyInstance): void {
       });
 
       connection.socket.on("close", () => {
-        serialPort.close();
+        serialPort.close((err) => {
+          if (err) {
+            request.log.warn("something went wrong when closing the connection");
+            request.log.warn(`${err?.name}: ${err?.message}`);
+          } else {
+            request.log.info("the websocket connection has closed - serial port closed")
+          }
+        });
       });
 
       serialPort.on("close", () => {
         connection.socket.close();
+        request.log.info("the serial port has closed - connection closed");
       });
     } else {
       connection.socket.close(WSCustomCloseCode.PARAM_INVALID);
+      request.log.info("received invalid connection parameters - connection closed");
     }
   });
 }
