18d6eee96448c88fe7709a1a4c5e59973816dd06
Yi-Jyun Pan <pan93412@gmail.com>
2021-04-29 05:54:57 +0800

    feat(main): prevent CORS problem (temporary solution)

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import Fastify from "fastify";
 import fastifyWebsocket from "fastify-websocket";
+import fastifyCors from "fastify-cors";
 import controllers from "./src/controllers";
 
 const fastify = Fastify({
@@ -9,6 +10,7 @@ const fastify = Fastify({
 });
 
 fastify.register(fastifyWebsocket);
+fastify.register(fastifyCors);
 
 // Declare a route
 controllers.forEach((c) => c(fastify, {}));
@@ -16,7 +18,7 @@ controllers.forEach((c) => c(fastify, {}));
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(3000);
+    await fastify.listen(3001);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
